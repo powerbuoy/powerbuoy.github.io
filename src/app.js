c1824fa7cb7a38cb36c4d3b5d9b44b3e542d56bb
AL <powerbuoy@gmail.com>
2017-03-03 01:05:04 +0100

    MMkyea

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import {inject} from 'aurelia-framework';
 import {EventAggregator} from 'aurelia-event-aggregator';
+import {Http} from 'resources/services/http';
 
-@inject(EventAggregator)
+@inject(EventAggregator, Http)
 export class App {
-	constructor (ea) {
+	constructor (ea, http) {
 		this.ea = ea;
+		this.http = http;
 
 		// Add loading class
 		this.loadingSubscription = this.ea.subscribe('router:navigation:processing', event => {
@@ -15,6 +17,40 @@ export class App {
 		this.navigationSubscription = this.ea.subscribe('router:navigation:success', event => {
 			document.documentElement.classList.remove('loading');
 		});
+	}
+
+	attached () {
+		// Random unsplash image
+		let unsplashKey = '4d6fb906f11c473986d67a17526e0949ed4f6b084565793cd88dd4615167cc62';
+
+		this.http.get('https://api.unsplash.com/photos/random?client_id=' + unsplashKey).then(data => {
+			this.setBg(data);
+		}).catch(error => {
+			this.setBg({
+				urls: {regular: 'scripts/assets/bg/bg.jpg'},
+				user: {
+					urls: {html: 'https://unsplash.com/@grakozy'},
+					name: 'Greg Rakozy'
+				}
+			});
+		});
+	}
+
+	setBg (data) {
+		let style = document.createElement('style');
+
+		style.type = 'text/css';
+		style.innerHTML = '.text--cutout, body {background-image: url(' + data.urls.regular + ')}';
+
+		document.getElementsByTagName('head')[0].appendChild(style);
+
+		// Credit text
+		let credit = document.createElement('p');
+
+		credit.classList.add('photo-credit');
+		credit.innerHTML = 'Photo by <a href="' + data.user.urls.html + '">' + data.user.name + '</a>';
+
+		document.body.appendChild(credit);
 	}
 
 	configureRouter (config, router) {
--- src/resources/attributes/smooth-scroll.js
+++ src/resources/attributes/smooth-scroll.js
@@ -14,13 +14,7 @@ export class SmoothScrollCustomAttribute {
 
 	attached () {
 		this.el.addEventListener('click', this.onClick);
-
-		// Find the target element
 		this.target = document.getElementById(this.el.getAttribute('href').substr(1));
-
-		// Find the scroll parent of the target element
-		this.scrollParent = this.findScrollableParent(this.target);
-		this.scrollParent = this.scrollParent == document.documentElement ? false : this.scrollParent;
 	}
 
 	detached () {
@@ -30,20 +24,7 @@ export class SmoothScrollCustomAttribute {
 	scroll () {
 		velocity(this.target, 'scroll', {
 			duration: 800,
-			container: this.scrollParent
+			offset: -100
 		});
-	}
-
-	findScrollableParent (node) {
-		if (node === null) {
-			return null;
-		}
-
-		if ((node.scrollHeight - 10) > node.clientHeight) {
-			return node;
-		}
-		else {
-			return this.findScrollableParent(node.parentNode);
-		}
 	}
 }
